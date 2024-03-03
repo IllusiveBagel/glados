aaf5dfb8385a5b4fca4254eb9bf331cc668b6900
logan.young <logan.young@autoprotectgroup.co.uk>
2024-03-03 17:11:42 +0000

    Slimmed down glados python client

--- glados.py
+++ glados.py
@@ -1,27 +1,18 @@
 import os
-import json
-import random
-
-from gladosTTS import speak
-from skills import home_assistant as ha;
-from skills import commands as cmd;
+from os.path import exists
 from pocketsphinx import LiveSpeech;
 import speech_recognition as sr;
+import requests;
+import yaml;
+from playsound import playsound
+from tempfile import NamedTemporaryFile
 
-# Get json of phrases
-file = open("./phrases.json")
-phrases = json.load(file)
-
-def start_up():
-    ha.initialize()
-    speak("oh, its you")
-    print("\nWaiting for keyphrase: ")
+settings = "config.yaml"
+headers = {
+    'Content-Type': 'application/json'
+}
 
 def take_command():
-    greetings = phrases["greetings"]
-    greetLen = len(greetings)
-
-    speak(greetings[random.randint(0, greetLen - 1)])
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -32,13 +23,36 @@ def take_command():
     try:
         print("Recognizing...")
         query = r.recognize_google(audio, language='en').lower()
+        print(f"User said: {query}")
     except Exception as e:
         print(e)
         print("Say that again please...")
         return "None"
     return query
 
-start_up()
+# Check and load the YAML settings file
+def validate_settings(load=False):
+	global settings
+
+	# Check if YAML file exists
+	if not exists(settings):
+		print("\033[1;31mERROR 1:\033[1;97m "+settings+" file not found.")
+		return False
+
+
+	# Check if YAML is valid and load it to RAM
+	with open(settings, "r") as stream:
+		try:
+			settings = yaml.safe_load(stream)
+		except yaml.YAMLError as exc:
+			print("\033[1;31mERROR 2:\033[1;97m Error parsing "+settings+" file:\n")
+			print(exc)
+			return False
+          
+# Check for setting YAML
+if (exists(settings)):
+    # Validate and load
+    validate_settings(load=True)
 
 speech = LiveSpeech(
     lm=False,
@@ -50,7 +64,9 @@ speech = LiveSpeech(
 for phrase in speech:
     try:
         command = take_command()
-
-        cmd.process_command(command)
+        response = requests.post(settings["url"] + "/command", headers=headers, data='{"command":"'+command+'"}', stream=True)
+        with NamedTemporaryFile(delete=False, suffix='.wav') as f:
+            f.write(response.content)
+            playsound(f.name)
     except Exception as e:
         print(e)
--- gladosTTS.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('./glados_tts')
-
-from glados_tts.glados import tts_runner;
-
-glados = tts_runner(False, True)
-
-def speak(text):
-    glados.speak(text, True)
